acacddfff17654286bc3cda121fda9e43970a49b
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2024-02-08 22:03:22 -0500

    routes, seeds, db,config files added

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const routes = require('./routes');
-require('dotenv').config();
+require('dotenv').config(); 
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
 // import sequelize connection
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
